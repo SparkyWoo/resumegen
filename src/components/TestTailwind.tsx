aaec8e947b48b1aff4ea331322764b0ce0a8fc48
Sang Park <fbmath@gmail.com>
2024-12-13 17:07:56 -0800

    remove test tailwind

--- src/components/TestTailwind.tsx
+++
@@ -1,9 +0,0 @@
-export const TestTailwind = () => {
-  return (
-    <div className="p-4">
-      <div className="rounded-lg bg-red-500 p-4 text-white">
-        If you see this in white text with red background and rounded corners, Tailwind is working!
-      </div>
-    </div>
-  );
-}; 
